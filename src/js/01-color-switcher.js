14313c79c16d3383b95b7749ef549287ceaa7531
Tanya Kyrylenko <nanalenko1905@gmail.com>
2022-11-23 03:46:24 +0200

    додала розмітку

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,17 @@
+  const startBtn = document.querySelector('[data-start]');
+  const stopBtn = document.querySelector('[data-stop]');
+  let timer = null;
+  startBtn.addEventListener('click', () => {
+    timer = setInterval(() => {
+      document.body.style.background = getRandomHexColor();
+    }, 1000);
+    startBtn.disabled = true;
+  });
+  stopBtn.addEventListener('click', () => {
+    clearInterval(timer);
+    startBtn.disabled = false;
+  });
 
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,60 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('[name=delay]'),
+  step: document.querySelector('[name=step]'),
+  amount: document.querySelector('[name=amount]'),
+  button: document.querySelector('button'),
+  
 }
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+
+function createPromise(position, delay) {
+  return new Promise((fulfill, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+     
+      if (shouldResolve) {
+        fulfill({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+ let evtDelay = Number(refs.delay.value);
+let evtStep = Number(refs.step.value);
+  let evtAmount = Number(refs.amount.value);
+  
+console.log('delay:', evtDelay); 
+console.log('step:', evtStep); 
+  console.log('amount:', evtAmount); 
+  
+  
+  for (let i = 1; i <= evtAmount; i++) {
+
+   
+    createPromise(i, evtDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+       })
+    
+      .catch(({ position, delay }) => {
+     Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    
+    evtDelay += evtStep;
+   
+    }
+}
+ 
